979aa22aef6d8af4186c0a888eb4abbe6466b25b
Ayushtripathy <tripathyayush123@gmail.com>
2021-08-10 00:28:57 +0530

    datepicker added

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Image from "next/image";
 import {
   SearchIcon,
@@ -6,8 +7,26 @@ import {
   MenuIcon,
   UsersIcon,
 } from "@heroicons/react/solid";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { DateRangePicker } from "react-date-range";
 
 const Header = () => {
+  const [searchInput, setSearchInput] = useState("");
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+
+  function handleSelect(ranges) {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  }
+
   return (
     <header
       className="sticky top-0 z-50 grid grid-cols-3
@@ -28,6 +47,10 @@ const Header = () => {
 
       <div className="flex items-center  sm:border-2 rounded-full py-2 md:shadow-sm hover:border-red-400">
         <input
+          value={searchInput}
+          onChange={(e) => {
+            setSearchInput(e.target.value);
+          }}
           className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600
            placeholder-gray-400"
           type="text"
@@ -55,6 +78,19 @@ const Header = () => {
           <UserCircleIcon className="h-6" />
         </div>
       </div>
+      {searchInput && (
+        <div className="flex flex-col col-span-3 mx-auto">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            onChange={handleSelect}
+            minDate={new Date()}
+            rangeColors={["#FD5B61"]}
+          />
+          <div>
+            <h2>Number of Guests</h2>
+          </div>
+        </div>
+      )}
     </header>
   );
 };
